b5ec9a1a7286c553f7b5562e911b8aaa8a40b027
Daniele Di Salvo <disi91@hotmail.it>
2020-11-26 10:36:12 +0100

    UPDATE class RadixSort

--- src/main/java/Radix/RadixSort.java
+++ src/main/java/Radix/RadixSort.java
@@ -1,9 +1,14 @@
 package Radix;
 
+import java.util.Arrays;
+
 public class RadixSort {
     public static void main(String[] args) {
         int[] testData = {26, 14, 105, 15, 16, 3, 13};
-        System.out.println(java.util.Arrays.toString(radixSort(testData, 3))); // [3, 13, 14, 15, 16, 26, 105]
+        System.out.println(Arrays.toString(radixSort(testData, 3))); // [3, 13, 14, 15, 16, 26, 105]
+
+        int[] test = {1, 5, 7, 9, 11, 13, 14, 15, 2, 3, 4, 6, 8, 10, 12}; // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        System.out.println(Arrays.toString(radixSort(test, 2)));
     }
 
     /*
