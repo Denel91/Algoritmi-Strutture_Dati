687c542a8c5f1f3c38697a3e1daaf7658df1a683
Daniele Di Salvo <disi91@hotmail.it>
2020-12-18 20:38:23 +0100

    IMPLEMENTED treeInsert method

--- src/main/java/Binary_Search_Tree/BinarySearchTree.java
+++ src/main/java/Binary_Search_Tree/BinarySearchTree.java
@@ -17,7 +17,7 @@ public class BinarySearchTree {
     // Pre-Order print of a tree
     public void preorderPrint(Node n) {
         if (n != null) {
-            System.out.println(n.key);
+            System.out.print(n.key + " ");
             preorderPrint(n.left);
             preorderPrint(n.right);
         }
@@ -27,7 +27,7 @@ public class BinarySearchTree {
     public void inorderPrint(Node n) {
         if (n != null) {
             inorderPrint(n.left);
-            System.out.println(n.key);
+            System.out.print(n.key + " ");
             inorderPrint(n.right);
         }
     }
@@ -37,7 +37,50 @@ public class BinarySearchTree {
         if (n != null) {
             postorderPrint(n.left);
             postorderPrint(n.right);
-            System.out.println(n.key);
+            System.out.print(n.key + " ");
         }
+    }
+
+    /**
+     * Insert a new Node in the BinarySearchTree
+     *
+     * @param z the new node to add
+     */
+    public void treeInsert(Node z) {
+        Node y = null;
+        Node x = root;
+        while (x != null) {
+            y = x;
+            if (z.key < x.key) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+
+        z.parent = y;
+        if (y == null) {
+            root = z;
+        } else if (z.key < y.key) {
+            y.left = z;
+        } else {
+            y.right = z;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(12);
+        root.addLeft(new Node(5));
+        root.addRight(new Node(18));
+        root.left.addLeft(new Node(2));
+        root.left.addRight(new Node(9));
+        root.right.addLeft(new Node(15));
+        root.right.addRight(new Node(19));
+        root.right.left.addRight(new Node(17));
+
+        Node z = new Node(13);
+        BinarySearchTree tree = new BinarySearchTree(root);
+        tree.treeInsert(z);
+        tree.preorderPrint(root); // 12 5 2 9 18 15 13 17 19
     }
 }
