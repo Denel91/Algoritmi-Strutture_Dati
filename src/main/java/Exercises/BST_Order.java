1921329a28fc7d4c455d09fbc0cf5fa587d30f22
Daniele Di Salvo <disi91@hotmail.it>
2020-12-30 20:44:10 +0100

    INSERT sort() method

--- src/main/java/Exercises/BST_Order.java
+++ src/main/java/Exercises/BST_Order.java
@@ -14,6 +14,10 @@ public class BST_Order {
         System.out.println(Arrays.toString(v)); // [12, 5, 2, 9, 18, 15, 13, 17, 19]
         sortWithBST(v);
         System.out.println(Arrays.toString(v)); // [2, 5, 9, 12, 13, 15, 17, 18, 19]
+
+        int[] vector = {6, 3, 2, 5, 7, 8};
+        sort(vector);
+        System.out.println(Arrays.toString(vector));
     }
 
     /**
@@ -25,8 +29,8 @@ public class BST_Order {
         // Empty BinarySearchTree
         BinarySearchTree T = new BinarySearchTree();
 
-        for (int i = 0; i < A.length; i++) {
-            Node x = new Node(A[i]);
+        for (int j : A) {
+            Node x = new Node(j);
             T.treeInsert(x);
         }
 
@@ -51,5 +55,39 @@ public class BST_Order {
     public static void inOrder(BinarySearchTree T, int[] A) {
         Node x = T.getRoot();
         inOrderNode(x, A, 0);
+    }
+
+    /**
+     * Ordina un Vettore A utilizzando un BinarySearchTree
+     *
+     * @param A un vettore di Integers.
+     */
+    public static void sort(int[] A) {
+        assert A != null : "A non può essere null";
+        // Empty BinarySearchTree
+        BinarySearchTree T = new BinarySearchTree();
+        for (int j : A) {
+            Node x = new Node(j);
+            T.treeInsert(x);
+        }
+
+        sortSuccessor(T, A);
+    }
+
+    /**
+     * Procedura di supporto che riempie l'array A in ordine
+     *
+     * @param A un vettore di Integers.
+     * @param T un BinarySearchTree
+     */
+    private static void sortSuccessor(BinarySearchTree T, int[] A) {
+        int pos = 0;        // indice iniziale dell'array A
+        int n = A.length;   // lunghezza dell'array A
+        Node min = T.treeMin(T.getRoot()); // calcolo il minimo
+        while (pos < n) {
+            A[pos] = min.getKey();
+            pos++;
+            min = T.treeSuccessor(min); // calcolo il successore del minimo
+        }
     }
 }
